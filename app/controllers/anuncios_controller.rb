fb14ac81248af350a2ac5cc251dd53122f8591a7
Nykolas Laurentino de Lima <nykolas.lima@gmail.com>
2013-05-07 22:16:17 -0300

    adding Anuncio update support

--- app/controllers/anuncios_controller.rb
+++ app/controllers/anuncios_controller.rb
@@ -9,4 +9,17 @@ class AnunciosController < ApplicationController
 
 		redirect_to root_url
 	end
+
+	def edit
+		@anuncio = Anuncio.find params[:id]
+	end
+
+	def update
+		anuncio = Anuncio.find params[:id]
+		if anuncio.update_attributes params[:anuncio]
+			flash[:notice] = "Anúncio atualizado."
+		end
+
+		redirect_to root_url
+	end
 end

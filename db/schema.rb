0a9a60a4db165d407bea6fcdda90d661512ecaab
Nykolas Laurentino de Lima <nykolas.lima@gmail.com>
2013-05-09 19:53:30 -0300

    adding role attribute to User

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130509011049) do
+ActiveRecord::Schema.define(:version => 20130509225135) do
 
   create_table "anuncios", :force => true do |t|
     t.string   "modelo"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(:version => 20130509011049) do
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
     t.string   "nome"
+    t.string   "role"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
